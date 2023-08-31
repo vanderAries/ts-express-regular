48c8dcf243d338d429b15a2c4d83c2200b7d9c0a
vanderAries <gp.bak96@gmail.com>
2023-08-31 11:24:11 +0200

    Add basic error handling to controllers

--- src/app/controllers/tasks.ts
+++ src/app/controllers/tasks.ts
@@ -6,93 +6,134 @@ import { TaskRequest } from '../models/tasks';
 
 const createTask = (req: Request, res: Response) => {
   try {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      const error: ErrorResponse = {
+    const validationError = validationResult(req);
+    if (!validationError.isEmpty()) {
+      const errorRes: ErrorResponse = {
         title: 'Validation Error',
         detail:
           "Provided request body is invalid, check 'errors' for more info.",
-        errors: result.array(),
+        errors: validationError.array(),
       };
 
-      return res.status(400).json(error);
+      return res.status(400).json(errorRes);
     }
 
     const taskInput = req.body as TaskRequest;
 
     const newTask = taskService.createTask(taskInput);
     return res.status(201).json(newTask);
-  } catch {
-    const error: ErrorResponse = {
+  } catch (error) {
+    console.log('Error in taskController:', error);
+    const errorRes: ErrorResponse = {
       title: 'Internal Server Error',
       detail:
         'Server encountered an unexpected problem',
     };
-    return res.status(500).json(error);
+    return res.status(500).json(errorRes);
   }
 };
 
 const getAllTasks = (req: Request, res: Response) => {
-  const tasks = taskService.getAllTasks();
-  res.status(200).json(tasks);
+  try {
+    const tasks = taskService.getAllTasks();
+    return res.status(200).json(tasks);
+  } catch (error) {
+    console.log('Error in taskController:', error);
+    const errorRes: ErrorResponse = {
+      title: 'Internal Server Error',
+      detail:
+        'Server encountered an unexpected problem',
+    };
+    return res.status(500).json(errorRes);
+  }
 };
 
 const getTaskById = (req: Request, res: Response) => {
-  const taskId = req.params.id;
-  const task = taskService.getTaskById(taskId);
-  if (task === undefined) {
-    const error: ErrorResponse = {
-      title: 'Not Found',
-      detail: `Task with ID: '${taskId}' was not found.`,
+  try {
+    const taskId = req.params.id;
+    const task = taskService.getTaskById(taskId);
+    if (task === undefined) {
+      const errorRes: ErrorResponse = {
+        title: 'Not Found',
+        detail: `Task with ID: '${taskId}' was not found.`,
+      };
+      return res.status(404).json(errorRes);
+    }
+    return res.status(200).json(task);
+  } catch (error) {
+    console.log('Error in taskController:', error);
+    const errorRes: ErrorResponse = {
+      title: 'Internal Server Error',
+      detail:
+        'Server encountered an unexpected problem',
     };
-    return res.status(404).json(error);
+    return res.status(500).json(errorRes);
   }
-  return res.status(200).json(task);
 };
 
 const updateTask = (req: Request, res: Response) => {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    const error: ErrorResponse = {
-      title: 'Validation Error',
+  try {
+    const validationError = validationResult(req);
+    if (!validationError.isEmpty()) {
+      const errorRes: ErrorResponse = {
+        title: 'Validation Error',
+        detail:
+          "Provided request body is invalid, check 'errors' for more info.",
+        errors: validationError.array(),
+      };
+      // check if it's better to use 'return' or 'else' statement
+      return res.status(400).json(errorRes);
+    }
+
+    const taskId = req.params.id;
+    const taskInput = req.body as TaskRequest;
+
+    const updatedTask = taskService.updateTask(taskId, taskInput);
+
+    if (updatedTask === undefined) {
+      const errorRes: ErrorResponse = {
+        title: 'Not Found',
+        detail: `Task with ID: '${taskId}' was not found.`,
+      };
+      return res.status(404).json(errorRes);
+    }
+
+    return res.status(200).json(updatedTask);
+  } catch (error) {
+    console.log('Error in taskController:', error);
+    const errorRes: ErrorResponse = {
+      title: 'Internal Server Error',
       detail:
-        "Provided request body is invalid, check 'errors' for more info.",
-      errors: result.array(),
+        'Server encountered an unexpected problem',
     };
-    // check if it's better to use 'return' or 'else' statement
-    return res.status(400).json(error);
+    return res.status(500).json(errorRes);
   }
-
-  const taskId = req.params.id;
-  const taskInput = req.body as TaskRequest;
-
-  const updatedTask = taskService.updateTask(taskId, taskInput);
-
-  if (updatedTask === undefined) {
-    const error: ErrorResponse = {
-      title: 'Not Found',
-      detail: `Task with ID: '${taskId}' was not found.`,
-    };
-    return res.status(404).json(error);
-  }
-
-  return res.status(200).json(updatedTask);
 };
 
 const deleteTask = (req: Request, res: Response) => {
-  const taskId = req.params.id;
+  try {
+    const taskId = req.params.id;
 
-  const deletedTask = taskService.deleteTask(taskId);
+    const deletedTask = taskService.deleteTask(taskId);
 
-  if (deletedTask === undefined) {
-    const error: ErrorResponse = {
-      title: 'Not Found',
-      detail: `Task with ID: '${taskId}' was not found.`,
+    if (deletedTask === undefined) {
+      const errorRes: ErrorResponse = {
+        title: 'Not Found',
+        detail: `Task with ID: '${taskId}' was not found.`,
+      };
+      return res.status(404).json(errorRes);
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log('Error in taskController:', error);
+    const errorRes: ErrorResponse = {
+      title: 'Internal Server Error',
+      detail:
+        'Server encountered an unexpected problem',
     };
-    return res.status(404).json(error);
+    return res.status(500).json(errorRes);
   }
-
-  return res.sendStatus(204);
 };
 
 export default {
